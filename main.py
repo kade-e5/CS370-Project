cfb834f84f659cbf373bd6c65a0e7e2c0e0f0b1b
omarwrs <142950133+omarwrs@users.noreply.github.com>
2023-11-18 12:43:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from flask import Flask, render_template, Response, request, send_from_directory
 from camera import VideoCamera
 import os
 
+camera_pi = Camera(flip=False) #Camera is not upside down set flip to false
+
 def webpage():
   return render_template('Webpage.html') #rendering the Webpage for the camera display
