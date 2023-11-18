8d16fc807fdbfc47349d3b6e300ac59a5ff1cabe
omarwrs <142950133+omarwrs@users.noreply.github.com>
2023-11-18 12:26:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from flask import Flask, render_template, Response, request, send_from_directory
 from camera import VideoCamera
 import os
 
-
+def webpage():
+  return render_template('Webpage.html') #rendering the Webpage for the camera display
