d1f49f4c9f84934d83d01dd331114d255d169872
omarwrs <142950133+omarwrs@users.noreply.github.com>
2023-11-18 12:46:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ import os
 
 camera_pi = Camera(flip=False) #Camera is not upside down set flip to false
 
+def gen(camera):
+    #get camera frame
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 def webpage():
   return render_template('Webpage.html') #rendering the Webpage for the camera display
