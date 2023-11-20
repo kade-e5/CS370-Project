d6d1a2d63936b169c5dc8cf108dc56e8764e9131
omarwrs <142950133+omarwrs@users.noreply.github.com>
2023-11-20 13:10:31 -0700

    Update cameraStream.py

--- cameraStream.py
+++ cameraStream.py
@@ -6,13 +6,11 @@ from datetime import datetime
 import numpy as np
 
 class Camera(object): 
-  def __init__(self, flip = False, file_type  = ".jpg", filename= "camera_photo"):
-        # self.vs = PiVideoStream(resolution=(1920, 1080), framerate=30).start()
-        self.vs = PiVideoStream().start()
-        self.flip = flip # Flip frame vertically
-        self.file_type = file_type # image type i.e. .jpg
-        self.filename = filename # Name to save the photo
-        time.sleep(2.0)
+  def __init__(self, file_type  = ".jpg", filename= "camera_photo"):
+        self.vs = PiVideoStream(framerate=60).start()
+        self.file_type = file_type 
+        self.filename = filename 
+        
     
   def __del__(self):
       self.vs.stop()
@@ -20,5 +18,4 @@ class Camera(object):
   def get_frame(self):
         frame = self.vs.read()
         ret, jpeg = cv.imencode(self.file_type, frame)
-        self.last_frame = jpeg
         return jpeg.tobytes()
