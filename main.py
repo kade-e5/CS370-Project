6722f7fb9c4f12604d9044bc723512f36dac8ac2
omarwrs <142950133+omarwrs@users.noreply.github.com>
2023-11-20 13:14:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template, Response, request, send_from_directory
 from cameraStream import Camera
 import os
 
-camera_pi = Camera(flip=False) #Camera is not upside down set flip to false
+camera_pi = Camera() 
 
 # App Globals
 app = Flask(__name__)
 
 @app.route('/')
 def webpage():
-  return render_template('Webpage.html') #rendering the Webpage for the camera display
+  return render_template('index.html') #rendering the Webpage for the camera display
 
 def gen(cameraStream):
     #get camera frame
@@ -21,6 +21,12 @@ def gen(cameraStream):
 @app.route('/video_feed')
 def video_stream():
     return Response(gen(camera_pi),boundarytype='multipart/x-mixed-replace; boundary=frame')
+
+# Take a photo when pressing camera button
+@app.route('/picture')
+def take_picture():
+    pi_camera.take_picture()
+    return "None"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
